e9d4630ff3e322063b63e868c54808a08c0b7aa0
Abishevs <eduards@abishevs.dev>
2024-04-27 11:42:40 +0200

    finnish line

--- simulations/integral-calc.py
+++ simulations/integral-calc.py
@@ -1,33 +1,47 @@
+from typing import Tuple
 import sympy as sp
 
 x = sp.symbols('x')
 a_v = sp.symbols('a')
 b_v = sp.symbols('b')
 
-function = (95000 * 2 * x * sp.pi) / (x**2 + 10 * x + 16)
 
-indefinite_integral = sp.integrate(function, x)
-print("Indefinite Integral:", indefinite_integral)
+def calc_indefinate_integral(function, x):
+    return sp.integrate(function, x)
 
-# a = a_v
-# b = b_v 
-# definite_integral = sp.integrate(function, (x, a, b))
-# print("Definite Integral from", a, "to", b, ":", definite_integral)
+def calc_definate_integral(function, 
+                      values: Tuple[float,float, float]):
+    """Takes in a function e.g. x^2
+        values: x, a, b
+        returns integral value as float
+    """
+    res = sp.integrate(function, (values))
+    return res.evalf()
 
-a = 0  
-b = 5 
-definite_integral = sp.integrate(function, (x, a, b))
-print("Definite Integral from", a, "to", b, ":", definite_integral)
+if __name__ == "__main__":
+    function = (95000 * 2 * x * sp.pi) / (x**2 + 10 * x + 16)
 
-# To get a floating-point number if necessary
-numeric_result = definite_integral.evalf()
-print("Numerical result:", numeric_result)
+    print("Indefinite Integral:", calc_indefinate_integral(function, x))
 
-a = 5  
-b = 10 
-definite_integral = sp.integrate(function, (x, a, b))
-print("Definite Integral from", a, "to", b, ":", definite_integral)
+    # a = a_v
+    # b = b_v 
+    # definite_integral = sp.integrate(function, (x, a, b))
+    # print("Definite Integral from", a, "to", b, ":", definite_integral)
 
-# To get a floating-point number if necessary
-numeric_result = definite_integral.evalf()
-print("Numerical result:", numeric_result)
+    a = 0  
+    b = 5 
+    definite_integral = sp.integrate(function, (x, a, b))
+    print("Definite Integral from", a, "to", b, ":", definite_integral)
+
+    # To get a floating-point number if necessary
+    numeric_result = definite_integral.evalf()
+    print("Numerical result:", numeric_result)
+
+    a = 5  
+    b = 10 
+    definite_integral = sp.integrate(function, (x, a, b))
+    print("Definite Integral from", a, "to", b, ":", definite_integral)
+
+    # To get a floating-point number if necessary
+    numeric_result = definite_integral.evalf()
+    print("Numerical result:", numeric_result)
